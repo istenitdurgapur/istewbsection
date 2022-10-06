b338de3ec2daab2274ffc432eaa10c0428b7d118
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-10-06 12:49:47 +0530

    models created

--- website/home/admin.py
+++ website/home/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
 
+from home.models import Contact, Event, Gallery, PostBearer
+
 # Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(PostBearer)
+admin.site.register(Event)
+admin.site.register(Gallery)
