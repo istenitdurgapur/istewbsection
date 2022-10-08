75cbf653c3f4c9ce3a4e786258136432d3705057
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-10-09 00:42:17 +0530

    backend connected for announcements , gallery , contact us and events improved

--- website/home/views.py
+++ website/home/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from . import models
 
 def home(request):
-    return render(request, 'home.html',{})
+    events_data = models.Event.objects.all().values()
+    return render(request, 'home.html',{'data': events_data})
 
 def about(request):
     return render(request, 'about.html',{})
 
 def gallery(request):
-    return render(request, 'gallery.html',{}) 
+    data = models.Gallery.objects.all().values()
+    return render(request, 'gallery.html',{'data': data}) 
 
 def events(request):
     events_data = models.Event.objects.all().values()
     return render(request, 'events.html',{'data': events_data}) 
 
 def contact(request):
-    return render(request, 'contact-us.html',{})
+    contact_data = models.Contact.objects.all().values()
+    return render(request, 'contact-us.html',{'data':contact_data})
