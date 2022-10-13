c5458e258d7ff525e2f4e0254aec6b1ae534c205
Harshit Surana <harsh007js@gmail.com>
2022-10-14 02:12:56 +0530

    Changes in Models.py for Events and Posts

--- website/home/admin.py
+++ website/home/admin.py
@@ -1,13 +1,15 @@
 
 from django.contrib import admin
 
-from home.models import Contact, Event, Gallery, PostBearer, Announcement, carousel
+from home.models import Contact, Event, Gallery, PostBearer, Announcement, carousel, Post, Chapter
 
 # Register your models here.
 
 admin.site.register(Contact)
+admin.site.register(Post)
 admin.site.register(PostBearer)
 admin.site.register(Event)
-admin.site.register(Gallery)
 admin.site.register(Announcement)
+admin.site.register(Gallery)
 admin.site.register(carousel)
+admin.site.register(Chapter)
--- website/home/models.py
+++ website/home/models.py
@@ -1,67 +1,107 @@
-from datetime import datetime
 from django.db import models
-
-# Create your models here.
-
+from django.utils import timezone
 
 class Gallery(models.Model):
   title = models.CharField(max_length=100)
   image=models.ImageField(upload_to='images/gallery')
-  date = models.DateTimeField(default=datetime.today())
+  date = models.DateField(default=timezone.localdate)
+
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
   
   def __str__(self):
          return self.title
+
 class carousel(models.Model):
   title = models.CharField(max_length=100)
   image=models.ImageField(upload_to='images/gallery')
-  date = models.DateTimeField(default=datetime.today())
+  date = models.DateField(default=timezone.localdate)
+
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+  
+  def __str__(self):
+         return self.title
+class Event(models.Model):
+  title = models.CharField(max_length=100)
+  date = models.DateTimeField(default=timezone.now)
+  venue = models.CharField(max_length=100, default='Online')
+  description = models.TextField(max_length=5000,blank=True)
+  
+  image1=models.ImageField(upload_to='images/Events',null = True , blank=True)
+  image2=models.ImageField(upload_to='images/Events',null = True , blank=True)
+  image3=models.ImageField(upload_to='images/Events',null = True , blank=True)
+
+  title_link1 = models.CharField(max_length=200, default='', blank=True)
+  url_link1 = models.URLField(blank=True)
+  title_link2 = models.CharField(max_length=200, default='', blank=True)
+  url_link2 = models.URLField(blank=True)
+  title_link3 = models.CharField(max_length=200, default='', blank=True)
+  url_link3 = models.URLField(blank=True)
+  
+  title_file1 = models.CharField(max_length=200, default='', blank=True)
+  upload_file1 = models.FileField(upload_to='doc/',null = True , blank=True)
+  title_file2 = models.CharField(max_length=200, default='', blank=True)
+  upload_file2 = models.FileField(upload_to='doc/',null = True , blank=True)
+  title_file3 = models.CharField(max_length=200, default='', blank=True)
+  upload_file3 = models.FileField(upload_to='doc/',null = True , blank=True)
+
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
   
   def __str__(self):
          return self.title
 
-
-class Event(models.Model):
-  EventName = models.CharField(max_length=100)
-  eventDate = models.DateTimeField(default=datetime.today())
+class Announcement(models.Model):
+  title = models.CharField(max_length=100)
   description = models.TextField(max_length=5000,blank=True)
-  Image1=models.ImageField(upload_to='images/Events',null = True , blank=True)
-  Image2=models.ImageField(upload_to='images/Events',null = True , blank=True)
-  Image3=models.ImageField(upload_to='images/Events',null = True , blank=True)
-  file1 = models.FileField(upload_to='doc/',null = True , blank=True)
-  Link1 = models.CharField(max_length=5000,blank=True)
-  file2 = models.FileField(upload_to='doc/',null = True , blank=True)
-  Link2 = models.CharField(max_length=5000,blank=True)
-  file3 = models.FileField(upload_to='doc/',null = True , blank=True)
-  Link3 = models.CharField(max_length=5000,blank=True)
+  link = models.URLField(blank=True)
+
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
   
   def __str__(self):
-         return self.EventName
+         return self.title
 
 class Contact(models.Model):
   name = models.CharField(max_length=100)
-  phone1 = models.IntegerField(default=1234567890)
-  phone2 = models.IntegerField(default=1234567890)
+  phone1 = models.IntegerField(default=9999999999)
+  phone2 = models.IntegerField(default=9999999999)
   email = models.EmailField()
   location = models.TextField(max_length=5000,blank=True)
+
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
   
   def __str__(self):
          return self.name
 
-
+class Post(models.Model):
+  title = models.CharField(max_length=100) 
+  priority = models.IntegerField(default=0)
+  
+  def __str__(self):
+         return self.title + " (Priority - " + str(self.priority) + ")"
 class PostBearer(models.Model):
   name = models.CharField(max_length=100)
-  image=models.ImageField(upload_to='images/postBearer',null = True , blank=True)
-  Post = models.TextField(max_length=100,default=" ")
-  details = models.TextField(max_length=100,default=" ")
- 
+  post = models.ForeignKey('Post', null=True, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to='images/PostBearer',null = True, blank=True)
+  introduction = models.TextField(max_length=200,default=" ", blank=True, null=True)
+  description = models.TextField(max_length=5000,default=" ", blank=True, null=True)  
+
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
   
   def __str__(self):
-         return self.name
+         return "P " + str(self.post.priority) + " - " + str(self.post.title) + " - " + self.name
 
-class Announcement(models.Model):
-  Name = models.CharField(max_length=100)
-  Date = models.DateTimeField(default=datetime.today())
-  description = models.TextField(max_length=5000,blank=True)
-  Link1 = models.CharField(max_length=5000,blank=True,default='#')
+class Chapter(models.Model):
+  name = models.CharField(max_length=100)
+  chapter_code = models.CharField(max_length=100)
+  college = models.CharField(max_length=100)
+  address = models.TextField(max_length=500)
+  email = models.EmailField()
+
   def __str__(self):
-         return self.Name
+    return str(self.chapter_code) + " - " + str(self.name)
+
