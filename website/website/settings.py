e41524479aba9e5a0438fb8d487fcfb8517f1ac6
Harshit Surana <harsh007js@gmail.com>
2022-11-05 01:22:26 +0530

    X_Frame_Option to sameorigin

--- website/website/settings.py
+++ website/website/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'home',
 ]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
